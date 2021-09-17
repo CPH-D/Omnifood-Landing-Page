ad7025d16821828fdd58afddbb41eea241c61beb
Christopher Hernandez <cerberean661@gmail.com>
2021-09-16 19:44:45 -0700

    added js for auto closing mobile nav when clicked for link

--- js/script.js
+++ js/script.js
@@ -26,14 +26,19 @@ allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
+    // scroll to top
     if (href === "#") window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
+    // scroll to links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
+    };
+    // close mobile menu nav
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
   });
 });
 
